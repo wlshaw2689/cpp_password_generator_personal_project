f50976c6bb522eb368c0e0806ec258f2bf5afb8e
William Levern Shaw II <william.shawii96@gmail.com>
2018-10-10 21:22:38 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,10 @@
 #include <iostream>
+#include <string>
+#include <memory>
+#include "Password.h"
+
+//********Function Prototypes******//
+int print_menu();
 
 int main(){
     /*
@@ -7,13 +13,8 @@ int main(){
      * Alright so what do I want this password generator to do?
      * U
      * */
-     int choice;
-     std::cout << "Hello and welcome to the prototype build of the password generator\n";
-     std::cout << "As of right now, we have only two options.\n";
-     std::cout << "1: Create a new password\n" << 
-                            "2: Retrieve a password based on the name\n";
-    std::cout << "Please enter your choice: ";
-    std::cin >> choice;
+     int choice = print_menu();
+   
     /*
      * 
      * Going to test something first before I make my decision.
@@ -21,14 +22,42 @@ int main(){
      * Looping through a string?
      * Looping through an array of characters?
      * Looping through a vector of characters?
+     *
+     * After testing things, it seems there was no difference. A vector of characters in not needed. I will be going with the string
+     * Do I want to use resize?
      * */
     
     
     switch(choice){
-        case 1:
-                //Create the new password;
+        case 1:{
+                std::cout << "How long do you want your password to be?";
+                int length_of_password;
+                std::cin >> length_of_password;
+                std::cout << "What would you like the name of this password to be? ";
+                std::string name_of_password;
+                std::cin.ignore();
+                std::getline(std::cin, name_of_password);
+                std::unique_ptr<Password> password = std::make_unique<Password>(name_of_password, length_of_password);
+                std::cout << password->GetNameOfPassword() << std::endl;
+                std::cout << "Length of pasword: " << password->GetActualPassword().length() << std::endl;
+                break;
+        }
         case 2:
+            break;
                 //Retrieve existing password;
     }
     return 0;
+}
+
+
+int print_menu(){
+    int choice;
+    std::cout << "Hello and welcome to the prototype build of the password generator\n";
+    std::cout << "As of right now, we have only two options.\n";
+    std::cout << "1: Create a new password\n" << 
+                            "2: Retrieve a password based on the name\n";
+    std::cout << "Please enter your choice: ";
+    std::cin >> choice;
+    return choice;
+    
 }
